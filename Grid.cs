5032a6fd17afe2d77dd7c0822394f23f537eefff
Hetler <https://github.com/H3tler>
2024-01-25 16:47:55 +0200

    I acidentally created the most beautiful thing ever!

--- Grid.cs
+++ Grid.cs
@@ -51,22 +51,26 @@ namespace FallingSandSimulator
         public void Gravity()
         {
             float[,] newgrid = grid;
+            bool yn;
             
             for (int r = rows - 1; r >= 0; r--) {
                 for (int c = 0; c < cols; c++) {
+                    yn = false;
                     if (grid[r, c] > 0 && r < rows - 1 && grid[r + 1, c] == 0) {
-                        if (newgrid[r + 1, c] > 0) continue;
-                        newgrid[r + 1, c] = grid[r, c];
-                        newgrid[r, c] = 0f;
+                        if (newgrid[r + 1, c] > 0) 
+                        yn = true;
+                        newgrid[r + 1, c] = grid[r, c];                      
+                        newgrid[r, c] = 0f;     
+                        newgrid = PushToSide(newgrid, new Vector2(c, r + 1));                  
                     }
-                    else if (grid[r, c] > 0 && r < rows - 1 && grid[r + 1, c] > 0)
-                    {
-                        newgrid = PushToSide(newgrid, new Vector2(c, r));
-                    }
-                }
+                    // if (grid[r, c] > 0 && r < rows - 1 && grid[r + 1, c] > 0 && yn == true)
+                    // {
+                    //     newgrid = PushToSide(newgrid, new Vector2(c, r));
+                    // }
+                }                         
             }
 
-            grid = newgrid;             
+            grid = newgrid;         
         }
 
         private float[,] PushToSide(float[,] someGrid, Vector2 pos)
@@ -76,20 +80,40 @@ namespace FallingSandSimulator
             int y = (int)pos.Y;
             int x = (int)pos.X;
             
+            // if (val == 1) {
+            //     for (int j = cols - 1; j > x; j--) {
+            //         if (someGrid[y, j] == 0) {
+            //             someGrid[y, j] = someGrid[y, j - 1];
+            //             someGrid[y, j - 1] = 0;
+            //         }
+            //     }                
+            // }
+            // else {
+            //     for (int i = 0; i < x; i++) {
+            //         if (someGrid[y, i] == 0) {
+            //             someGrid[y, i] = someGrid[y, i + 1];
+            //             someGrid[y, i + 1] = 0;
+            //         }
+            //     }
+            // }
+
             if (val == 1) {
-                for (int j = cols - 1; j > x; j--) {
-                    if (someGrid[y, j] == 0) {
-                        someGrid[y, j] = someGrid[y, j - 1];
-                        someGrid[y, j - 1] = 0;
+                for (int i = x; i < cols - 1; i++) {
+                    if (someGrid[y, i + 1] == 0) {
+                        someGrid[y, i + 1] = someGrid[y, i];
+                        someGrid[y, i] = 0;
+                        return someGrid;
                     }
-                }                
-            }
+                    
+                }
+            }   
             else {
-                for (int i = 0; i < x; i++) {
-                    if (someGrid[y, i] == 0) {
-                        someGrid[y, i] = someGrid[y, i + 1];
-                        someGrid[y, i + 1] = 0;
-                    }
+                for (int i = x; i >= 1; i--) {
+                    if (someGrid[y, i - 1] == 0) {
+                        someGrid[y, i - 1] = someGrid[y, i];
+                        someGrid[y, i] = 0;
+                        return someGrid;
+                    }            
                 }
             }
             
